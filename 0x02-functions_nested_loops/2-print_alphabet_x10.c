c8186acb7350b6f7f6e6cccc1e998b45250273c0
Christiano-alx <christianowebdev@gmail.com>
2022-07-05 18:15:41 -0700

    worked on three files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.c""
+
+/**
+ * Prints numbers - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
