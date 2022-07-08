2bb8620fa821ec84bfee0d7439f8e0d2e7d299c0
Christiano-alx <christianowebdev@gmail.com>
2022-07-08 14:37:38 -0700

    Done with te project

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: length of the line
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putcahr(' ');
+		}
+		_putchar(92);
+		if (a < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
