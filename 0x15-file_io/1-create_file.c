6b650b9bde2a246895899cf2e859c6a1d09a5d6a
Christiano-alx <christianowebdev@gmail.com>
2022-08-22 08:33:35 -0700

    Fixing Errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
-	int nwr;
+	int rwr;
 
 	if (!filename)
 		return (-1);
@@ -28,9 +28,9 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	nwr = write(fd, text_content, nletters);
+	rwr = write(fd, text_content, nletters);
 
-	if (nwr == -1)
+	if (rwr == -1)
 		return (-1);
 
 	close(fd);
